94acd7b6cbd796f28655a992769d9b30a7775d9c
Daniel Sperling-Horowitz <dwshorowitz@gmail.com>
2014-10-08 03:11:14 -0400

    Syncing w/ Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = 'e5ea55d17d66808300eeac8b54a815f7808fd60cf356c0beeafc7d31033292233fc83fdb92e6db1c5f5e3f170bcfb53940ccc90a685c76420c6af911527576c9'
