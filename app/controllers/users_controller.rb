b89346f2b1510304694b9c837d654b577fa223d0
Daniel Sperling-Horowitz <dwshorowitz@gmail.com>
2014-10-17 18:07:46 -0400

    ready for signin and signout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to @user
-      flash[:success] = "Hello hello, welcome to Bubble!"
+      flash[:success] = "Welcome to Bubble!"
     else
       render 'new'
     end
