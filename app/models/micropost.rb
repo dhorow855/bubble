1927e41a93b63695dd2b68282bb8fcb2951e8130
Daniel Sperling-Horowitz <dwshorowitz@gmail.com>
2014-10-30 01:59:38 -0400

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,14 @@ class Micropost < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   validates :content, presence: true, length: { maximum: 100 }
   validates :user_id, presence: true
+
+  # Returns microposts from the users being followed by the given user.
+  # As site grows, we probably need to generate the feed asynchronously using 
+  # a background job.
+  def self.from_users_followed_by(user)
+  	followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+  	where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+  		  user_id: user.id)
+  end
 end
